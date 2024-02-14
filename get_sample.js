a404277d29b7d80225cb2bba1620bb5888071511
kanoloa <kanoloa@gmail.com>
2024-02-14 10:03:32 +0900

    added several functions such as 'getDownStream()'.

--- get_sample.js
+++ get_sample.js
@@ -3,10 +3,12 @@
 //         at Feb 10, 2024
 
 const cb = require('./lib/cbclient');
+const constants = require("constants");
 
 async function main() {
     try {
 
+        /*
         // example 1:
         // get list of projects
         const projects = await cb.getProjects();
@@ -26,6 +28,34 @@ async function main() {
         const fields = await cb.getItemById("5410");
         for (const field of fields.editableFields) {
             console.log(`fieldID: ${field.fieldId}, name: ${field.name}, value: ${field.value}`);
+        }
+
+
+        // example 4:
+        // get child nodes.
+        const children = await cb.getChildren("2171");
+        console.log(children);
+        for (const child of children.itemRefs) {
+            console.log(`id: ${child.id},  name: ${child.name}`)
+        }
+         */
+
+        // example 5:
+        // get downstream references.
+        // this method returns an array.
+        const downstream = await cb.getDownstream("2171");
+        console.log("--- DOWN STREAMS ---");
+        for (const down of downstream) {
+            console.log(`id: ${down.id}, type: ${down.type}`);
+        }
+
+        // example 6:
+        // get upstream references.
+        // this method returns an array.
+        const upstream = await cb.getUpstream("2171");
+        console.log("--- UP STREAMS ---");
+        for (const up of upstream) {
+            console.log(`id: ${up.id}, type: ${up.type}`);
         }
 
     } catch(e) {
